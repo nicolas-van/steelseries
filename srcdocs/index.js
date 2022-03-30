64a5f9395437e9cc8cd5ffaf3a8d38d3ebf1ae64
Nicolas Vanhoren <nicolas.vanhoren@gmail.com>
2022-03-31 00:07:32 +0200

    Made doc work again

--- srcdocs/index.js
+++ srcdocs/index.js
@@ -1,6 +1,6 @@
 
 import { generateDocumentation } from './doc-generator.js'
-import '@vanillawc/wc-markdown/index.js'
+import '@vanillawc/wc-markdown'
 
 const AltimeterDoc = generateDocumentation('steelseries-altimeter')
 window.customElements.define('steelseries-doc-altimeter', AltimeterDoc)
